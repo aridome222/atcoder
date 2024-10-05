7a63ceda3c25d56a7271ca7169acce7703ddc225
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-10-05 23:08:21 +0900

    update: chminとchmaxマクロ関数の追加

--- join_abc_beginner_contest/create_abcFolder_atcoder.sh
+++ join_abc_beginner_contest/create_abcFolder_atcoder.sh
@@ -46,6 +46,26 @@ using namespace std;
         }                            \
         cout << endl;                \
     }
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
 
 int main()
 {
