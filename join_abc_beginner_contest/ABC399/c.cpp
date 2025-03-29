b1e893ebfb6d95fb6fb78e748401348e40724f10
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2025-03-29 23:33:48 +0900

    add: ABC399 Cまで完答（keyword: 相対順位・同立順位, DFS・連結成分数）

---
+++ join_abc_beginner_contest/ABC399/c.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define rep3(i, m, n) for (int i = (m); i < (int)(n); ++i)
+#define repR(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
+#define rep3R(i, m, n) for (int i = (int)(n) - 1; i >= (int)(m); --i)
+#define all(x) std::begin(x), std::end(x)
+#define rAll(x) std::rbegin(x), std::rend(x)
+#define contain(str, ch) (str.find(ch) != string::npos)
+#define mPrint(msg) cout << msg << endl;
+#define arrPrint(arr)                \
+    {                                \
+        bool first = true;           \
+        for (const auto &elem : arr) \
+        {                            \
+            if (!first)              \
+                cout << " ";         \
+            cout << elem;            \
+            first = false;           \
+        }                            \
+        cout << endl;                \
+    }
+#define dump(x) cout << #x << " = " << (x) << endl;
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+bool isOK(const vector<T> &a, size_t index, T key)
+{
+    if (a[index] >= key)
+        return true;
+    else
+        return false;
+}
+template <class T>
+int binary_search(const vector<T> &a, T key)
+{
+    size_t left = 0;
+    size_t right = a.size();
+
+    while (right - left > 1)
+    {
+        size_t mid = left + (right - left) / 2;
+
+        if (isOK(a, mid, key))
+            right = mid;
+        else
+            left = mid;
+    }
+
+    return right;
+}
+using Graph = vector<vector<int>>;
+vector<bool> seen;
+void dfs(const Graph &g, int v)
+{
+    seen[v] = true; // v を訪問済にする
+
+    // v から行ける各頂点 next_v について
+    for (auto next_v : g[v])
+    {
+        if (seen[next_v])
+            continue;   // next_v が探索済だったらスルー
+        dfs(g, next_v); // 再帰的に探索
+    }
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    Graph g(n);
+    rep(i, m)
+    {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    seen.assign(n, false);
+    int unit_cnt = 0;
+    rep(v, n)
+    {
+        if (!seen[v])
+        {
+            dfs(g, v);
+            unit_cnt++;
+        }
+    }
+
+    int calc = n - unit_cnt;
+    mPrint(m - calc);
+
+    return 0;
+}
